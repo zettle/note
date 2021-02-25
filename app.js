65bc8b7e9e114bbb4934d94f53a7948a6de34ee6
zettle <719925519@qq.com>
2021-02-25 08:41:33 +0800

    'dist'

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ function resolve (filePath) {
     return path.resolve(__dirname, filePath);
 }
 
-console.log('NODE_ENV', process.env.NODE_ENV);
-
 md2html({
-    publicPath: process.env.NODE_ENV=== 'production' ? 'node' : 'dist', // 生产note， 本地dist 
+    publicPath: process.env.NODE_ENV=== 'production' ? 'note' : 'dist', // 生产note， 本地dist
     source: resolve('./src'),
     output: resolve('./dist'),
     mdConf: {}
 });
-
