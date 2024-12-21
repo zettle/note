3b49f4fa32cfc6cf8b57d13ace89f52c8a682e60
huangzt5 <2394024809@qq.com>
2024-12-21 21:31:58 +0800

    feat: 加自动脚本

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import VitePluginVitepressDemo from 'vite-plugin-vitepress-demo';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/note/',
     // glob: './**/demo/**/*.{vue,jsx,tsx,js,ts}' 这里表示扫描所有demo目录下的vue,jsx,tsx,js,ts作为demo示例代码
     plugins: [VitePluginVitepressDemo({ glob: './articles/**/demo/**/*.{vue,jsx,tsx,js,ts}' })],
     server: {
